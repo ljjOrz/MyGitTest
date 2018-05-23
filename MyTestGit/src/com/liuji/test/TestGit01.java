1d732e5cf11dfc2a36c9807dd92b2c2db889ab8c
liuji <liujizuihao@163.com>
2018-05-23 10:42:04 +0800

    增加项目

---
+++ MyTestGit/src/com/liuji/test/TestGit01.java
@@ -0,0 +1,8 @@
+package com.liuji.test;
+
+public class TestGit01 {
+	
+	public static void main(String[] args) {
+		System.out.println("123");
+	}
+}
---
+++ MyTestGit/src/com/liuji/test/TestGit02.java
@@ -0,0 +1,9 @@
+package com.liuji.test;
+
+public class TestGit02 {
+
+	public static void main(String[] args) {
+		System.out.println("1234");
+	}
+	
+}
